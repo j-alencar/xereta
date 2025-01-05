d4cc42d3f1795f68ae5d5aabe612a6ff3bedd7cf
j-alencar <joao.alencar@univille.br>
2025-01-05 01:30:19 -0300

    feat: request do JSON real via first = elemento "total"

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,6 +4,7 @@ from lib.drivers import SeleniumWireDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from io import BytesIO
+import requests
 import gzip
 import json
 
@@ -25,12 +26,28 @@ class ScraperEnjoei(Scraper):
         super().__init__("https://enjoei.com.br", SeleniumWireDriver().get_driver())
 
     def search(self, query):
-        driver = self.driver
-        driver.get(self.search_url)
+        self.driver.get(self.search_url)
 
-        search_box = driver.find_element(By.CLASS_NAME, "search-input__input")
+        search_box = self.driver.find_element(By.CLASS_NAME, "search-input__input")
         search_box.send_keys(query)
         search_box.send_keys(Keys.RETURN)
+
+    def get_total(self, request):
+        compressed_body = BytesIO(request.response.body)
+                    
+        with gzip.GzipFile(fileobj=compressed_body) as f:
+            decompressed_body = f.read()
+
+        temp_json = json.loads(decompressed_body)
+
+        total = temp_json['data']['search']['products']['total'] # número real de resultados
+        return total
+
+    def get_true_json(self, request):
+        total = self.get_total(request)
+        true_url = request.url.replace("first=30", f"first={total}")
+        r = requests.get(true_url)
+        return r.json()
 
     def get_products(self, query):
         self.search(query)
@@ -38,14 +55,8 @@ class ScraperEnjoei(Scraper):
         while True:
             for request in self.driver.requests:
                 if parse_filename(request) == "graphql-search-x" and request.method == "GET" and request.response.status_code == 200:
-                    compressed_body = BytesIO(request.response.body)
-                    
-                    with gzip.GzipFile(fileobj=compressed_body) as f:
-                        decompressed_body = f.read()
-                    payload = json.loads(decompressed_body.decode('utf-8')) # acentos
-
                     self.driver.quit()
-                    return json.dumps(payload, indent=4, ensure_ascii=False)
-                
-    #TODO: paginação
-                            
+
+                    return self.get_true_json(request)
+                else:
+                    continue                            
